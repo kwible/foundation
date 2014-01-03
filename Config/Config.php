d6a3723a40a4645ddffac916f6506dfe993c9ce3
Kwible Components <wiebe.van.breukelen@xs4all.nl>
2014-01-03 18:18:08 +0100

    Removed some rubbish code
    Sublime Text plugin has added some rare things to the file.

--- Config/Config.php
+++ Config/Config.php
@@ -21,7 +21,6 @@ class Config
 
 	public static function get($resource)
 	{
-<<<<<<< HEAD
 		foreach (self::$resources as $configCollection)
 		{
 			foreach ($configCollection as $configItemName => $configItem) {
@@ -29,13 +28,11 @@ class Config
 				{
 					return $configItem;
 				}
-=======
 		foreach (self::$resources as $configResource)
 		{
 			if (in_array($resource, $configResource))
 			{
 				return $configResource[$resource];
->>>>>>> 67f38b5d7ca6afeae7b4a6f41160c8cca4fda232
 			}
 		}
 
